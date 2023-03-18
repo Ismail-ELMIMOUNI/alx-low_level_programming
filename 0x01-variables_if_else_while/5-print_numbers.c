9c3cf5e122907c3e894a577bb12624c97316aa28
Ismail-ELMIMOUNI <ismail.elmimouni94@gmail.com>
2023-03-17 19:57:17 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,10 @@
  */
 int main(void)
 {
-	int i;
+	int n;
 
-	for (i = 0; i < 10; i++)
-	{
-		putchar(i + '0');
-		putchar('\n');
-	}
+	for (n = 0; n < 10; n++)
+		printf("%d\n", n);
 
 	return (0);
 }
