ad289e80a9b5c083a51cef3a770dee5cbf831107
Ismail-ELMIMOUNI <ismail.elmimouni94@gmail.com>
2023-03-20 18:27:59 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n < 9; n++)
+	{
+		for (m = n + 1; m < 10; m++)
+		{
+			putchar(n + '0');
+			putchar(m + '0');
+
+			if (n != 8 || m != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
