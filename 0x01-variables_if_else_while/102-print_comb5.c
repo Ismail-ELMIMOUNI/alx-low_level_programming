b93327ec6ccb1d28ebea17f7767dc23dcc9297aa
Ismail-ELMIMOUNI <ismail.elmimouni94@gmail.com>
2023-03-20 19:54:02 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,27 +7,26 @@
  */
 int main(void)
 {
-	int i, j, k;
+	int i, j;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = i + 1; j < 9; j++)
+		for (j = i; j < 100; j++)
 		{
-			for (k = j + 1; k < 10; k++)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(k + '0');
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
 
-				if (i != 7 || j != 8 || k != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+			if (i < 99 || j < 99)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
+
