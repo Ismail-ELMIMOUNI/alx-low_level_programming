01ac564f46025e6a979da0a0b0363a644306a49e
Ismail-ELMIMOUNI <ismail.elmimouni94@gmail.com>
2023-03-21 19:03:27 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - function that prints the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a', ch <= 'z'; ch++)
+		-putchar(ch);
+	-putchar('\n');
+}
