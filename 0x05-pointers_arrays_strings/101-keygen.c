f6f64e1b131a1c5bc2d85f58d369c690dee5df63
Ismail-ELMIMOUNI <ismail.elmimouni94@gmail.com>
2023-03-27 19:44:41 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,18 +9,25 @@
  */
 int main(void)
 {
-	char password[7];
-	int i;
+	int pass[100];
+	int i, sum, n;
 
+	sum = 0;
 	srand(time(NULL));
-
-	for (i = 0; i < 6; i++)
+	for (i = 0; i < 100; i++)
 	{
-		password[i] = rand() % 94 + 33;
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
-	password[6] = '\0';
-
-	printf("%s\n", password);
 
 	return (0);
 }
+
