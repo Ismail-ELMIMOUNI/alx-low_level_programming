30ce22637d756349e52521daf853bf1b9ac0d884
Ismail-ELMIMOUNI <ismail.elmimouni94@gmail.com>
2023-04-03 19:18:43 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * set_string - Sets the value of a pointer to a char.
+ * @s: Pointer to a pointer to a char.
+ * @to: Pointer to a char.
+ */
+void set_string(char **s, char *to)
+{
+	int i;
+
+	/* Free memory allocated for *s */
+	free(*s);
+
+	/* Get length of to */
+	for (i = 0; to[i]; i++)
+		continue;
+
+	/* Allocate memory for *s with the same length as to */
+	*s = malloc(sizeof(char) * (i + 1));
+
+	/* Copy to to *s */
+	for (i = 0; to[i]; i++)
+		(*s)[i] = to[i];
+
+	(*s)[i] = '\0';
+}
